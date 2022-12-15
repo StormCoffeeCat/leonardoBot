26c63baca2d84a3048f4b86395d033f4e3c5244f
StormCoffeeCat <noah.smillie@outlook.com>
2022-12-15 23:30:58 +0000

    commit

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import os
 import openai
 import discord
-from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -15,47 +14,66 @@ openai.api_key = os.getenv('KEY')
 # discord setup
 bot = commands.Bot(command_prefix='./', intents=discord.Intents.all(), help_command=None)
 
+
 @bot.event
 async def on_ready():
-  await tree.sync()
-  print('Bot is ready')
+    await tree.sync()
+    print('Bot is ready')
+
 
 tree = bot.tree
 
+
 # define your commands using the @tree.command decorator
 @tree.command(name='ping', description='Pong!')
-async def ping(interaction: discord.Interaction, message:str):
+async def ping(interaction: discord.Interaction, message: str):
     await interaction.response.send_message(f'Pong! {message}')
     print(message)
 
-@tree.command(name='setup', description='Setup the bot by selecting what channel(s) you want it to talk in!')
-async def setup(interaction: discord.Interaction, channel: discord.TextChannel):
-    await interaction.response.send_message(f'Bot will now talk in {channel.mention}')
-    print(channel)
 
+@tree.command(name='setup', description='Setup the bot by selecting what channel you want it to talk in, and the '
+                                        'temperature (randomness) of the AI.')
+async def setup(interaction: discord.Interaction, channel: str, temperature: int):
+    await interaction.response.send_message(f'Bot will now talk in {channel} with a temperature of {temperature}')
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
+
 # bot checks for message
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
+    global temperature
+    global channel
+
     prompt = message.content
 
-    # prompts the ai to generate a response
-    completion = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        max_tokens=100,
-        temperature=0.8)
+    if message.author == bot.user:
+        return
+    if message.content.startswith('./'):
+        await bot.process_commands(message)
+        return
+    if message.content.startswith('!'):
+        return
+    if message.channel.name == channel:
 
-    # sends the response to the channel
-    await message.channel.send(completion.choices[0].text)
-    await bot.process_commands(message)
+        # prompts the ai to generate a response
+        completion = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            max_tokens=50,
+            temperature=temperature,
+        )
+
+        # sends the response to the channel
+        await message.channel.send(completion.choices[0].text)
+        await bot.process_commands(message)
+
+    else:
+        return
+
 
 # run bot
 bot.run(os.getenv('TOKEN'))
